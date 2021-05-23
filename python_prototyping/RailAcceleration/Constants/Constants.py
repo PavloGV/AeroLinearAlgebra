a6fdbea0d4e139555cadfdb469b618db1a5e8c8f
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-05-23 14:44:54 -0700

    added constatns

--- python_prototyping/RailAcceleration/Constants/Constants.py
+++ python_prototyping/RailAcceleration/Constants/Constants.py
@@ -9,17 +9,32 @@
 # Accelerator Constants
 
 ###############################################################################
-# Planet Constants
+# Planet and General Plotting Constants
+radiusEarth = 6371000 # Earth's radius in meters
+
+xmin = -radiusEarth*3.0
+xmax =  radiusEarth*3.0
+
+ymin = -radiusEarth*3.0
+ymax =  radiusEarth*3.0
+
+zmin = -radiusEarth*3.0
+zmax =  radiusEarth*3.0
+
+###############################################################################
+# Timing
+dt = 0.05	  	# sample time in seconds
+t0 = 0.0 		# Start time of simulation in seconds
+tf = 100.0 		# Stop time of simulation in seconds
 
 ###############################################################################
 # Projectile Constants
-dt = 0.05	  # sample time in seconds
-mass = 1252.0 # projectile mass in kg
+mass = 1252.0 	# projectile mass in kg
 
 # Initial Earth-Centered Earth-Fixed (ECEF) coordinates
-x0 = -2705.678 # x-coordinate in km
-y0 = -4325.150 # y-coordinate in km
-z0 = 3815.106  # z-coordinate in km
+x0 = -2705678.0 # x-coordinate in m
+y0 = -4325150.0 # y-coordinate in m
+z0 = 3815106.0  # z-coordinate in m
 
 # State size
 stateSize = 18
